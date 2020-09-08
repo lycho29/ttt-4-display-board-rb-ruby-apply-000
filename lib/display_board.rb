5263bc1b410949852a7b5a04d995f237c9987da8
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-08 17:33:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" "," "," "," "," "," "," "," "," "]
+  
+def display_board(board)
+  puts " O |   | 0 "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts " 0 | 0 | 0 "
+end
+
